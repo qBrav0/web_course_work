565a4de9c7764bb77da479fa8aeba2badd0f5f3b
Vlad <125458772+qBrav0@users.noreply.github.com>
2023-05-10 10:00:13 +0300

    added pages

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .models import Category, Product
 from cart.forms import CartAddProductForm
 
@@ -12,12 +12,21 @@ def product_list(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    return render (request, 'shop/product/list.html', context={
+    paginator = Paginator(products, 12)  # Ограничение количества товаров на странице до 12
+    page_number = request.GET.get('page')
+    try:
+        page_obj = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        # Якщо параметр page не є цілим числом, виводимо першу сторінку
+        page_obj = paginator.get_page(1)
+    except EmptyPage:
+        # Якщо номер сторінки більший, ніж загальна кількість сторінок, виводимо останню сторінку
+        page_obj = paginator.get_page(paginator.num_pages)
+    return render(request, 'shop/product/list.html', context={
         'category': category,
         'categories': categories,
-        'products': products,
+        'products': page_obj,
     })
-
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     cart_product_form = CartAddProductForm()
