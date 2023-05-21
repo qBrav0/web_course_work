930afc46096750208e5ebd4751020a9a02523cd7
Vlad <125458772+qBrav0@users.noreply.github.com>
2023-05-21 13:14:58 +0300

    preorder form data

--- orders/forms.py
+++ orders/forms.py
@@ -7,3 +7,13 @@ class OrderCreateForm(forms.ModelForm):
         model = Order
         fields = ['first_name', 'last_name', 'email', 'city', 'address', 'postal_code']
 
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
+        super(OrderCreateForm, self).__init__(*args, **kwargs)
+        if user and user.is_authenticated:
+            self.fields['first_name'].initial = user.first_name
+            self.fields['last_name'].initial = user.last_name
+            self.fields['email'].initial = user.email
+            self.fields['city'].initial = user.city
+            self.fields['address'].initial = user.address
+            self.fields['postal_code'].initial = user.postal_code
